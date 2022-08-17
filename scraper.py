777d6eb70cd094f59a61fe8c913fcb4609c96a9f
Sean Leckie <46270602+seannleckie@users.noreply.github.com>
2022-08-17 02:30:15 -0400

    Refactor to Product class Additionally, run through children of "cell" divs to tie multiple products to the same product name (i.e. for different prices/sizes)

--- scraper.py
+++ scraper.py
@@ -9,10 +9,12 @@ options = Options()
 options.headless = True
 options.add_argument("--window-size=1920,1200")
 driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
-driver.get("https://dutchie.com/stores/tweed-london/products/vaporizers")
+driver.get("https://dutchie.com/stores/STORENAMEHERE")
 
+DUTCHIE_KEY_PRODUCT_CELL = "jTzrhU"
 DUTCHIE_KEY_PRODUCT_BRAND = "bUxuOp"
 DUTCHIE_KEY_PRODUCT_NAME = "kjymBK"
+DUTCHIE_KEY_PRODUCT_SIZE = "hYKiO"
 DUTCHIE_KEY_PRODUCT_PRICE = "hJFddt"
 DUTCHIE_KEY_PRODUCT_BUTTON_SINGLE = "zdtBd"
 DUTCHIE_KEY_PRODUCT_BUTTON_DOUBLE = "esCRpt"
@@ -22,37 +24,59 @@ DUTCHIE_KEY_PAGE_BUTTON = "cwWhSO"
 DUTCHIE_KEY_PAGE_NEXT = "hjQwsb"
 DUTCHIE_KEY_PAGE_PREV = "deZqfc"
 
+productList = []
+
+
+class Product:
+    def __init__(self, brand, name, sizes, prices):
+        self.brand = brand
+        self.name = name
+        self.sizes = sizes
+        self.prices = prices
+
+    def toString(self):
+        return self.brand.ljust(30, ' ') + self.name.ljust(30, ' ')
+
+
 def dutchie_get_num_pages():
-	page_buttons = driver.find_elements(By.CLASS_NAME, DUTCHIE_KEY_PAGE_BUTTON)
-	return int(page_buttons[-1].text)
+    page_buttons = driver.find_elements(By.CLASS_NAME, DUTCHIE_KEY_PAGE_BUTTON)
+    if len(page_buttons) == 0:
+        return 0
+    return int(page_buttons[-1].text) - 1
 
-for page in range(dutchie_get_num_pages()):
-	print("\n\nPage " + str(page + 1) + "\n\n")
-	scrollLevel = 0
 
-	height = int(driver.execute_script("return document.documentElement.scrollHeight"))
+for page in range(dutchie_get_num_pages() + 1):
+    print("Processing page " + str(page + 1))
+    scrollLevel = 0
 
-	while scrollLevel < height:
-		scrollLevel += 1000
-		driver.execute_script("window.scrollTo(0," + str(scrollLevel) + ")")
-		time.sleep(0.2)
+    height = int(driver.execute_script("return document.documentElement.scrollHeight"))
 
-	# TODO: Some items have multiple sizes available. These will currently
-	# mess with the dataset, as the current design only accounts for one price
-	# per product
-	item_names = driver.find_elements(By.CLASS_NAME, DUTCHIE_KEY_PRODUCT_NAME)
-	item_prices = driver.find_elements(By.CLASS_NAME, DUTCHIE_KEY_PRODUCT_PRICE)
+    while scrollLevel < height:
+        scrollLevel += 1000
+        driver.execute_script("window.scrollTo(0," + str(scrollLevel) + ")")
+        time.sleep(0.2)
 
-	if (len(item_names) < len(item_prices)):
-		print("\n\nWARNING: More prices found than items. It appears there " +
-			"are some items with multiple sizes. The price display will be incorrect.\n\n")
+    cells = driver.find_elements(By.CLASS_NAME, DUTCHIE_KEY_PRODUCT_CELL)
 
-	for item in range(len(item_names)):
-		print(item_names[item].text + " - " + item_prices[item].text)
-	print("\n")
+    for index, cell in enumerate(cells):
+        product_brand = cell.find_element(By.CLASS_NAME, DUTCHIE_KEY_PRODUCT_BRAND).text
+        product_name = cell.find_element(By.CLASS_NAME, DUTCHIE_KEY_PRODUCT_NAME).text
+        product_sizes = cell.find_elements(By.CLASS_NAME, DUTCHIE_KEY_PRODUCT_SIZE)
+        product_prices = cell.find_elements(By.CLASS_NAME, DUTCHIE_KEY_PRODUCT_PRICE)
 
-	if page < dutchie_get_num_pages():
-		driver.find_element(By.CLASS_NAME, DUTCHIE_KEY_PAGE_NEXT).click()
-		time.sleep(3)
+        newProduct = Product(product_brand, product_name, product_sizes, product_prices)
+        productList.append(newProduct)
+        print(product_brand + " " + product_name)
+
+    if page < dutchie_get_num_pages():
+        driver.find_element(By.CLASS_NAME, DUTCHIE_KEY_PAGE_NEXT).click()
+        time.sleep(3)
+
+print(str(len(productList)) + " items added. Here is the list.")
+
+print("Brand".ljust(30, ' ') + "Name".ljust(30, ' '))
+
+for index, product in enumerate(productList):
+    print(productList[index].toString())
 
 driver.quit()
