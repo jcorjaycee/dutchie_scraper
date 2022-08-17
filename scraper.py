e1dc4178e54195ff1a75e1a4b7d3fa71480681f3
JC Leckie <jaycee@smime.ninja>
2022-08-17 09:17:59 -0400

    Revert timing values Now that #2 is resolved, setting values back to known good

--- scraper.py
+++ scraper.py
@@ -63,9 +63,9 @@ for page in range(NUM_PAGES + 1):
     height = int(driver.execute_script("return document.documentElement.scrollHeight"))
 
     while scrollLevel < height:
-        scrollLevel += 500
-        driver.execute_script("window.scrollTo(200," + str(scrollLevel) + ")")
-        time.sleep(0.4)
+        scrollLevel += 1000
+        driver.execute_script("window.scrollTo(500," + str(scrollLevel) + ")")
+        time.sleep(0.2)
 
     cells = driver.find_elements(By.CLASS_NAME, DUTCHIE_KEY_PRODUCT_CELL)
 
@@ -100,7 +100,7 @@ for page in range(NUM_PAGES + 1):
 
     if page < NUM_PAGES:
         driver.find_element(By.CLASS_NAME, DUTCHIE_KEY_PAGE_NEXT).click()
-        time.sleep(2)
+        time.sleep(1)
 
 print(str(len(productList)) + " items added. Here is the list.")
 
